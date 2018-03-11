89a56c4689a6efea75f486ac89851a210fa4825f
Ascendzor <wow.tmare@gmail.com>
2018-03-11 22:00:46 +1300

    init

---
+++ handler.js
@@ -0,0 +1,40 @@
+const AWS = require('aws-sdk')
+AWS.config.update({region: 'us-east-1'})
+const dynamoDb = new AWS.DynamoDB.DocumentClient()
+const uuid = require('uuid')
+
+// const body = {
+//   password: 'abceasyas123',
+//   email: 'abc@easy.as'
+// }
+module.exports.create = function(event, context, callback) {
+  const body = JSON.parse(event.body)
+  const params = {
+    TableName: 'correlation-profiles',
+    Item: body
+  }
+
+  dynamoDb.put(params, err => {
+    if(err) return callback(err)
+    callback(null, {
+      statusCode: 200,
+      body: JSON.stringify({message: 'your food has been saved'})
+    })
+  })
+}
+
+// const body = {
+//   password: 'abceasyas123'
+// }
+module.exports.get = function(event, context, callback) {
+  const {password} = event
+  if(!password) return callback({statusCode: 500, body: 'password was incorrect'})
+
+  dynamoDb.scan({TableName: 'correlation-profiles'}, (err, res) => {
+    if(err) return callback(err)
+    callback(null, {
+      statusCode: 200,
+      body: JSON.stringify(res.Items)
+    })
+  })
+}

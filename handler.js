8a2a1fb0b99adbf90ae82dbc5a3fe0f0a35e8843
Ascendzor <wow.tmare@gmail.com>
2018-03-13 17:39:03 +1300

    sign in works

--- handler.js
+++ handler.js
@@ -1,35 +1,36 @@
 const AWS = require('aws-sdk')
 AWS.config.update({region: 'us-east-1'})
+const secrets = require('./secrets')
+const Mailjet = require('node-mailjet').connect(secrets.thingy, secrets.thingy2);
+const jsonwebtoken = require('jsonwebtoken')
 const dynamoDb = new AWS.DynamoDB.DocumentClient()
-const uuid = require('uuid')
 
 // const body = {
 //   password: 'abceasyas123',
 //   email: 'abc@easy.as'
 // }
-module.exports.create = function(event, context, callback) {
-  const {password, email} = JSON.parse(event.body)
-  const params = {
-    TableName: 'correlation-profiles',
-    Item: {password, email}
-  }
+module.exports.signin = function(event, context, callback) {
+  const {email} = JSON.parse(event.body)
+  const token = jsonwebtoken.sign({profileId: email}, secrets.privateKey)
 
-  dynamoDb.put(params, err => {
-    if(err) return callback(err)
-    callback(null, {
-      statusCode: 200,
-      body: JSON.stringify({message: 'your food has been saved'})
-    })
+  Mailjet.post("send").request({
+    	FromEmail: 'development@bitbybit.nz',
+    	FromName: 'bitbybit',
+    	Subject: 'Sign in to Correlations',
+    	'Text-part': 'Hello, here is your sign in url http://localhost:3000/#jwt='+token,
+    	Recipients: [{'Email': email}]
+  }).then(res => {
+    callback(null, {statusCode: 200})
+  }).catch(err => {
+    console.log(err)
+    callback(null, {statusCode: 500})
   })
 }
 
-// const body = {
-//   password: 'abceasyas123'
-// }
 module.exports.get = function(event, context, callback) {
-  const {password} = JSON.parse(event.body)
-  console.log(event)
-  if(!password) return callback({statusCode: 500, body: 'password was incorrect'})
+  const {password} = event.queryStringParameters
+
+  if(!password) return callback(null, {statusCode: 500, body: 'password was incorrect'})
 
   dynamoDb.scan({TableName: 'correlation-profiles'}, (err, res) => {
     if(err) return callback(err)
